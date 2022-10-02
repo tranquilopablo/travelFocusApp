d8e985993fc87b604dab3294de7246a657a6651c
Pawel Zguda <zgudapawel@gmail.com>
2022-10-02 20:52:07 +0200

    create models

---
+++ api/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true}, 
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    places: { type: String, required: true },
+    
+});
+
+
+module.exports = mongoose.model('User', userSchema);
--- api/server.js
+++ api/server.js
@@ -5,15 +5,23 @@ const mongoose = require('mongoose');
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
 
-
 const app = express();
 
 app.use(bodyParser.json());
 
-
 app.use('/api/places', placesRoutes);
 app.use('/api/users', usersRoutes);
 
-app.listen(5000, () => {
-  console.log('Backend is running!');
-});
+mongoose
+  .connect(
+    `mongodb+srv://blunt17:bundy17@pawel.vs6xb.mongodb.net/travelFocusApp?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5000, () => {
+      console.log('Backend is runningg');
+      console.log('Connected to MongoDB');
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
